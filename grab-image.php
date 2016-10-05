3d69a06d749dbf39f6f32c21018cfcb2570268e3
Viet Tien Tang <viet.tang@niteco.se>
2016-10-05 14:51:06 +0700

    change version to 2.5

--- grab-image.php
+++ grab-image.php
@@ -2,7 +2,7 @@
 /**
  * @package grab-image
  * Plugin Name: Grab Image
- * Version: 2.4a
+ * Version: 2.5
  * Description: Grab images of img tags are re-uploads them to be located on the site.
  * Author: Niteco
  * Author URI: http://niteco.se/
